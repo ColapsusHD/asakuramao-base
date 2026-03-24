1c5d004d49036a980ed2182044c8ddb0bff8a270
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2026-03-24 06:34:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ const antilink = JSON.parse(fs.readFileSync('./settings/Grupo/Json/antilink.json
 const bngp = JSON.parse(fs.readFileSync('./settings/Grupo/Json/grupo.json'))
 const Antipv = JSON.parse(fs.readFileSync('./settings/Grupo/Json/chat.json'))
 const registro = JSON.parse(fs.readFileSync('./settings/Grupo/Json/registros.json')) 
-const Exportion = JSON.parse(fs.readFileSync('./Games/Json/exportion.json'))
-const Exportion1 = JSON.parse(fs.readFileSync('./Games/Json/exportion1.json'))
+// const Exportion = JSON.parse(fs.readFileSync('./Games/Json/exportion.json'))
+// const Exportion1 = JSON.parse(fs.readFileSync('./Games/Json/exportion1.json'))
 const Cuestions = JSON.parse(fs.readFileSync('./Games/Json/cuestions.json'))
               
    // 𝚃𝙸𝙼𝙴
